53491b092627e3302bd225cc3f248a27f322a208
Rushill Shah <rushill2@illinois.edu>
2023-05-10 02:21:27 -0500

    HyperBold applied on scroll, but spacing removed

--- content.js
+++ content.js
@@ -1,9 +1,10 @@
+
 function boldFirstCharacters() {
     const elements = document.querySelectorAll('p, blockquote, span');
     for (let i = 0; i < elements.length; i++) {
       const element = elements[i];
-      if (element.innerText.trim().length < 100) {
-        continue; // Skip elements with less than 40 characters
+      if (element.innerText.trim().length < 50) {
+        continue; // Skip elements with less than 50 characters
       }
   
       element.style.fontFamily = "Arial";
@@ -12,8 +13,8 @@ function boldFirstCharacters() {
       for (let j = 0; j < childNodes.length; j++) {
         const node = childNodes[j];
         if (node.nodeType === Node.TEXT_NODE && node.parentNode.tagName !== 'A') {
-            const words = node.textContent.trim().split(/\s+/, -1, 'preserveWhitespace');
-            for (let k = 0; k < words.length; k++) {
+          const words = node.textContent.trim().split(/\s+/);
+          for (let k = 0; k < words.length; k++) {
             const word = words[k];
             if (word.length <= 2) {
               continue; // Skip words with 2 or fewer characters
@@ -27,11 +28,11 @@ function boldFirstCharacters() {
             words[k] = `<strong>${firstChars}</strong>${remainingChars}`;
           }
           const newNode = document.createElement('span');
-          newNode.innerHTML = words.join(' ');
+          newNode.innerHTML = words.join('&nbsp;');
+          newNode.style.whiteSpace = 'pre-wrap';
           node.parentNode.replaceChild(newNode, node);
         }
       }
-      //   element.style.textAlign = 'justify'; // Justify the text in the element
   
       const strongElements = element.querySelectorAll('strong');
       for (let l = 0; l < strongElements.length; l++) {
@@ -48,12 +49,26 @@ function boldFirstCharacters() {
   
   const chatContainer = document.querySelector('.chat-container');
   if (chatContainer) {
-    chatContainer.addEventListener('click', function() {
-      boldFirstCharacters();
+    const observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        mutation.addedNodes.forEach(function(node) {
+          if (node.nodeType === Node.ELEMENT_NODE) {
+            boldFirstCharacters();
+          }
+        });
+      });
     });
+    observer.observe(chatContainer, { childList: true });
   }
   
-  window.addEventListener('scroll', function() {
-    boldFirstCharacters();
-  });
+  const observer = new IntersectionObserver(function(entries) {
+    entries.forEach(function(entry) {
+      if (entry.isIntersecting) {
+        boldFirstCharacters();
+        observer.unobserve(entry.target);
+        observer.observe(document.body);
+      }
+    });
+  }, { threshold: 0 });
   
+  observer.observe(document.body);
